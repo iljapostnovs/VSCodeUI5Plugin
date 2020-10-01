3ca60c9994065c86c0984b7fc6c81d106e39aef1
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2020-10-01 22:03:04 +0300

    add unused namespace xml linting

--- src/classes/Util/XMLLinter.ts
+++ src/classes/Util/XMLLinter.ts
@@ -58,6 +58,29 @@ export class XMLLinter {
 							}
 						}
 					});
+
+
+				}
+			}
+		});
+
+		const aPrefixes = document.match(/(?<=xmlns:).*?(?==)/g);
+		aPrefixes?.forEach(prefix => {
+			const aPrefixes = new RegExp(`(?<=<)${prefix}:`, "g").exec(document);
+			if (!aPrefixes || aPrefixes.length === 0) {
+
+				const positionBegin = document.indexOf(`xmlns:${prefix}=`);
+				const position = LineColumn(document).fromIndex(positionBegin - 1);
+				if (position) {
+					errors.push({
+						code: "UI5plugin",
+						message: "Unused namespace",
+						source: prefix,
+						range: new vscode.Range(
+							new vscode.Position(position.line - 1, position.col),
+							new vscode.Position(position.line - 1, position.col + "xmlns:".length + prefix.length)
+						)
+					});
 				}
 			}
 		});
