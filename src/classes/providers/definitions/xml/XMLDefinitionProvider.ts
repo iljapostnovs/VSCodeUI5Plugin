ad64c632217ee6230d26d6af02d65856e7bd6a87
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-01-22 11:32:03 +0200

    fix xml definition provider for inherited classes

--- src/classes/providers/definitions/xml/XMLDefinitionProvider.ts
+++ src/classes/providers/definitions/xml/XMLDefinitionProvider.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { UIClassFactory } from "../../../UI5Classes/UIClassFactory";
-import { CustomUIClass } from "../../../UI5Classes/UI5Parser/UIClass/CustomUIClass";
+import { CustomClassUIMethod, CustomUIClass } from "../../../UI5Classes/UI5Parser/UIClass/CustomUIClass";
 import { FileReader } from "../../../utils/FileReader";
 import LineColumn = require("line-column");
 import { XMLParser } from "../../../utils/XMLParser";
@@ -17,22 +17,37 @@ export class XMLDefinitionProvider {
 		const attribute = attributes?.find(attribute => XMLParser.getAttributeNameAndValue(attribute).attributeValue.replace(".", "") === word);
 		if (attribute) {
 			const { attributeValue } = XMLParser.getAttributeNameAndValue(attribute);
-			const controllerName = FileReader.getControllerNameFromView(XMLText);
+			let controllerName = FileReader.getControllerNameFromView(XMLText);
 			if (controllerName) {
-				const controllerUIClass = <CustomUIClass>UIClassFactory.getUIClass(controllerName);
-				const classPath = FileReader.convertClassNameToFSPath(controllerName, true);
-				const method = controllerUIClass.methods.find(method => method.name === attributeValue.replace(".", ""));
-				if (method?.position && classPath) {
-					const classUri = vscode.Uri.file(classPath);
-					const methodPosition = LineColumn(controllerUIClass.classText).fromIndex(method.position);
-					if (methodPosition) {
-						const vscodePosition = new vscode.Position(methodPosition.line - 1, methodPosition.col - 1);
-						location = new vscode.Location(classUri, vscodePosition);
+				controllerName = this._findClassNameOfEventHandler(controllerName, attributeValue)
+				if (controllerName) {
+					const controllerUIClass = UIClassFactory.getUIClass(controllerName);
+					if (controllerUIClass instanceof CustomUIClass) {
+						const classPath = FileReader.getClassPathFromClassName(controllerName);
+						const method = controllerUIClass.methods.find(method => method.name === attributeValue.replace(".", ""));
+						if (method?.position && classPath) {
+							const classUri = vscode.Uri.file(classPath);
+							const methodPosition = LineColumn(controllerUIClass.classText).fromIndex(method.position);
+							if (methodPosition) {
+								const vscodePosition = new vscode.Position(methodPosition.line - 1, methodPosition.col - 1);
+								location = new vscode.Location(classUri, vscodePosition);
+							}
+						}
 					}
 				}
 			}
 		}
 
 		return location;
+	}
+
+	private static _findClassNameOfEventHandler(className: string, methodName: string): string | undefined {
+		const UIClass = <CustomUIClass>UIClassFactory.getUIClass(className);
+		const method = UIClass.methods.find(method => method.name === methodName.replace(".", ""));
+		if (method) {
+			return className;
+		} else if (UIClass.parentClassNameDotNotation) {
+			return this._findClassNameOfEventHandler(UIClass.parentClassNameDotNotation, methodName);
+		}
 	}
 }
