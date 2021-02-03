2cda246fdd219f53c96d64303b1315a28bbf2460
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-02-03 22:47:55 +0200

    fix js code lens

--- src/classes/providers/codelens/jscodelens/JSCodeLensProvider.ts
+++ src/classes/providers/codelens/jscodelens/JSCodeLensProvider.ts
@@ -1,4 +1,7 @@
 import * as vscode from "vscode";
+import { CustomUIClass } from "../../../UI5Classes/UI5Parser/UIClass/CustomUIClass";
+import { UIClassFactory } from "../../../UI5Classes/UIClassFactory";
+import { FileReader } from "../../../utils/FileReader";
 import { EventHandlerCodeLensGenerator } from "./strategies/EventHandlerCodeLensGenerator";
 import { InternalizationTextCodeLenseGenerator } from "./strategies/InternalizationTextCodeLenseGenerator";
 import { OverridenMethodCodeLensGenerator } from "./strategies/OverridenMethodCodeLensGenerator";
@@ -7,6 +10,7 @@ export class JSCodeLensProvider {
 	static getCodeLenses(document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
 		return new Promise(resolve => {
 			setTimeout(() => {
+				this._setNewCodeForClassIfItIsChanged(document);
 				let codeLenses: vscode.CodeLens[] = [];
 
 				const aStrategies = [
@@ -22,5 +26,15 @@ export class JSCodeLensProvider {
 				resolve(codeLenses);
 			}, 0);
 		});
+	}
+
+	private static _setNewCodeForClassIfItIsChanged(document: vscode.TextDocument) {
+		const className = FileReader.getClassNameFromPath(document.fileName);
+		if (className) {
+			const UIClass = <CustomUIClass>UIClassFactory.getUIClass(className);
+			if (UIClass.classText.length !== document.getText().length) {
+				UIClassFactory.setNewCodeForClass(className, document.getText());
+			}
+		}
 	}
 }
