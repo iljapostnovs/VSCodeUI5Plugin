d0ba23e53a22a6b5dbc0a412d4f9e59e8e1760cb
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-05-17 21:08:03 +0300

    move proxy responsibility to axios

--- src/classes/utils/HTTPHandler.ts
+++ src/classes/utils/HTTPHandler.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
 import * as https from "https";
-import * as url from "url";
 import * as vscode from "vscode";
 export class HTTPHandler {
 	static async get(uri: string) {
@@ -13,42 +12,14 @@ export class HTTPHandler {
 		const options: AxiosRequestConfig = {
 			httpsAgent: agent
 		};
-		const proxy = process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
-		if (proxy) {
-			const parsedProxy = new url.URL(proxy);
-			if (parsedProxy.hostname) {
-				const port = this._getPort(parsedProxy);
-				options.proxy = {
-					host: parsedProxy.hostname,
-					port: port,
-					protocol: parsedProxy.protocol || undefined
-				};
-			}
-		}
 
 		try {
 			data = (await axios.get(uri, options)).data;
 		} catch (error) {
-			vscode.window.showErrorMessage(`Error occured sending HTTP Request. Message: "${error.message}". Response data: "${error.response?.data}"`);
+			vscode.window.showErrorMessage(`Error occurred sending HTTP Request. Message: "${error.message}". Response data: "${error.response?.data}"`);
 			throw error;
 		}
 
 		return data;
-	}
-
-	private static _getPort(uri: url.URL) {
-		let port;
-
-		if (uri.port) {
-			port = parseInt(uri.port);
-		} else if (uri.protocol === "http:") {
-			port = 80;
-		} else if (uri.protocol === "https:") {
-			port = 443;
-		} else {
-			port = 80;
-		}
-
-		return port;
 	}
 }
