6d6fafdc354ea2bc39dba07db3887c8dcc7df956
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2022-09-23 14:58:52 +0300

    add CustomTSClass

--- src/UI5Plugin.ts
+++ src/UI5Plugin.ts
@@ -13,6 +13,14 @@ import { JSRenameRegistrator } from "./classes/registrators/RenameRegistreator";
 import { TreeDataProviderRegistrator } from "./classes/registrators/TreeDataProviderRegistrator";
 import { UI5Parser, WorkspaceFolder } from "ui5plugin-parser";
 import { VSCodeParserConfigHandler } from "./classes/ui5parser/VSCodeParserConfigHandler";
+import { glob } from "glob";
+import * as path from "path";
+import * as ts from "typescript";
+import { readFileSync } from "fs";
+import { generateDocumentation } from "./typescript/Test";
+import watchMain = require("./typescript/Test");
+
+
 export class UI5Plugin {
 	private static _instance?: UI5Plugin;
 	public static pWhenPluginInitialized: Promise<void> | undefined;
@@ -32,33 +40,141 @@ export class UI5Plugin {
 		this.context?.subscriptions.push(disposable);
 	}
 	public initialize(context: vscode.ExtensionContext) {
-		UI5Plugin.pWhenPluginInitialized = new Promise<void>((resolve, reject) => {
-			vscode.window.withProgress({
-				location: vscode.ProgressLocation.Window,
-				title: "UI5Plugin",
-				cancellable: false
-			}, async progress => {
-				progress.report({
-					message: "Initializing...",
-					increment: 0
-				});
-				await this._initialize(context).then(() => {
+		if (this._getIsTypescriptProject()) {
+			// const delint = function(sourceFile: ts.SourceFile) {
+			// 	delintNode(sourceFile);
+
+			// 	function delintNode(node: ts.Node) {
+			// 		switch (node.kind) {
+			// 			case ts.SyntaxKind.ForStatement:
+			// 			case ts.SyntaxKind.ForInStatement:
+			// 			case ts.SyntaxKind.WhileStatement:
+			// 			case ts.SyntaxKind.DoStatement:
+			// 				if ((node as ts.IterationStatement).statement.kind !== ts.SyntaxKind.Block) {
+			// 					report(
+			// 						node,
+			// 						'A looping statement\'s contents should be wrapped in a block body.'
+			// 					);
+			// 				}
+			// 				break;
+
+			// 			case ts.SyntaxKind.IfStatement:
+			// 				const ifStatement = node as ts.IfStatement;
+			// 				if (ifStatement.thenStatement.kind !== ts.SyntaxKind.Block) {
+			// 					report(ifStatement.thenStatement, 'An if statement\'s contents should be wrapped in a block body.');
+			// 				}
+			// 				if (
+			// 					ifStatement.elseStatement &&
+			// 					ifStatement.elseStatement.kind !== ts.SyntaxKind.Block &&
+			// 					ifStatement.elseStatement.kind !== ts.SyntaxKind.IfStatement
+			// 				) {
+			// 					report(
+			// 						ifStatement.elseStatement,
+			// 						'An else statement\'s contents should be wrapped in a block body.'
+			// 					);
+			// 				}
+			// 				break;
+
+			// 			case ts.SyntaxKind.BinaryExpression:
+			// 				const op = (node as ts.BinaryExpression).operatorToken.kind;
+			// 				if (op === ts.SyntaxKind.EqualsEqualsToken || op === ts.SyntaxKind.ExclamationEqualsToken) {
+			// 					report(node, 'Use \'===\' and \'!==\'.');
+			// 				}
+			// 				break;
+			// 		}
+
+			// 		ts.forEachChild(node, delintNode);
+			// 	}
+
+			// 	function report(node: ts.Node, message: string) {
+			// 		const { line, character } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
+			// 		console.log(`${sourceFile.fileName} (${line + 1},${character + 1}): ${message}`);
+			// 	}
+			// }
+
+			// const workspaces =  vscode.workspace.workspaceFolders?.map(wsFolder => wsFolder.uri.fsPath) || [];
+			// const fileNames = workspaces.map(workspace => workspace + "\\src\\Component.ts");
+			// fileNames.forEach(fileName => {
+			// 	// Parse a file
+			// 	const sourceFile = ts.createSourceFile(
+			// 		fileName,
+			// 		readFileSync(fileName).toString(),
+			// 		ts.ScriptTarget.ES2015,
+			// 	  	true
+			// 	);
+
+			// 	// delint it
+			// 	delint(sourceFile);
+			// });
+			// class MyCompilerHost extends MyLanguageServiceHost implements ts.CompilerHost {
+			// 	getSourceFile(filename: string, languageVersion: ts.ScriptTarget, onError?: (message: string) => void): ts.SourceFile {
+			// 		var f = this.files[filename];
+			// 		if (!f) return null;
+			// 		var sourceFile = ts.createLanguageServiceSourceFile(filename, f.file, ts.ScriptTarget.ES5, f.ver.toString(), true, false);
+			// 		return sourceFile;
+			// 	}
+			// 	writeFile(filename: string, data: string, writeByteOrderMark: boolean, onError?: (message: string) => void): void {
+			// 	}
+			// 	getCanonicalFileName = (fileName: string) => fileName;
+			// 	useCaseSensitiveFileNames = () => true;
+			// 	getNewLine = () => "\n";
+			// }
+			const folderPaths = vscode.workspace.workspaceFolders?.map(wsFolder => wsFolder.uri.fsPath) || [];
+			watchMain(folderPaths[0]);
+
+			// const tsconfig = import("../tsconfig.json");
+			// generateDocumentation(components, {
+
+			UI5Plugin.pWhenPluginInitialized = new Promise((resolve) => resolve());
+		} else {
+			UI5Plugin.pWhenPluginInitialized = new Promise<void>((resolve, reject) => {
+				vscode.window.withProgress({
+					location: vscode.ProgressLocation.Window,
+					title: "UI5Plugin",
+					cancellable: false
+				}, async progress => {
 					progress.report({
 						message: "Initializing...",
-						increment: 100
+						increment: 0
 					});
-					resolve();
-				}).catch((error: any) => {
-					progress.report({
-						increment: 100
+					await this._initialize(context).then(() => {
+						progress.report({
+							message: "Initializing...",
+							increment: 100
+						});
+						resolve();
+					}).catch((error: any) => {
+						progress.report({
+							increment: 100
+						});
+						console.error(error);
+						reject("Couldn't initialize plugin: " + JSON.stringify(error.message));
 					});
-					console.error(error);
-					reject("Couldn't initialize plugin: " + JSON.stringify(error.message));
 				});
 			});
-		});
+		}
 
 		return UI5Plugin.pWhenPluginInitialized;
+	}
+	private _getIsTypescriptProject() {
+		const escapedFileSeparator = "\\" + path.sep;
+		const workspaceFolders = vscode.workspace.workspaceFolders?.map(wsFolder => {
+			return new WorkspaceFolder(wsFolder.uri.fsPath);
+		});
+
+		const components = workspaceFolders?.flatMap(wsFolder => {
+			const oConfigHandler = new VSCodeParserConfigHandler();
+			const wsFolderFSPath = wsFolder.fsPath.replace(new RegExp(`${escapedFileSeparator}`, "g"), "/");
+			const exclusions: string[] = oConfigHandler.getExcludeFolderPatterns();
+			const exclusionPaths = exclusions.map(excludeString => {
+				return `${wsFolderFSPath}/${excludeString}`
+			});
+			return glob.sync(`${wsFolderFSPath}/**/Component.ts`, {
+				ignore: exclusionPaths
+			});
+		})
+
+		return !!components?.length || false;
 	}
 	private async _initialize(context: vscode.ExtensionContext) {
 		const globalStoragePath = context.globalStorageUri.fsPath;
---
+++ src/typescript/parsing/classes/CustomTSClass.ts
@@ -0,0 +1,207 @@
+import ts = require("typescript");
+import { ICacheable } from "ui5plugin-parser/dist/classes/UI5Classes/abstraction/ICacheable";
+import {
+	AbstractUIClass,
+	IUIField,
+	IUIMethod
+} from "ui5plugin-parser/dist/classes/UI5Classes/UI5Parser/UIClass/AbstractUIClass";
+import { IViewsAndFragmentsCache } from "ui5plugin-parser/dist/classes/UI5Classes/UI5Parser/UIClass/CustomUIClass";
+
+export interface UI5Ignoreable {
+	ui5ignored?: boolean;
+}
+
+export interface IXMLDocumentMentionable {
+	mentionedInTheXMLDocument?: boolean;
+}
+
+export interface ITSNodeBearer<NodeType> {
+	tsNode: NodeType;
+	memberPropertyNode?: any;
+}
+export interface ICustomClassTSMethod
+	extends IUIMethod,
+	ITSNodeBearer<ts.MethodDeclaration>,
+	IXMLDocumentMentionable,
+	UI5Ignoreable {
+	position?: number;
+	isEventHandler: boolean;
+	acornParams?: any;
+}
+export interface ICustomClassTSField
+	extends IUIField,
+	ITSNodeBearer<ts.PropertyDeclaration>,
+	IXMLDocumentMentionable,
+	UI5Ignoreable {
+	customData?: Record<string, any>;
+}
+
+export class CustomTSClass extends AbstractUIClass implements ICacheable, ITSNodeBearer<ts.ClassDeclaration> {
+	readonly methods: ICustomClassTSMethod[] = [];
+	readonly fields: ICustomClassTSField[] = [];
+	private readonly _cache: Record<string, any> = {};
+	readonly fsPath: string | undefined;
+	readonly classText: string;
+	readonly relatedViewsAndFragments?: IViewsAndFragmentsCache[];
+	readonly tsNode: ts.ClassDeclaration;
+	private readonly _sourceFile: ts.SourceFile;
+	constructor(classDeclaration: ts.ClassDeclaration, sourceFile: ts.SourceFile, typeChecker: ts.TypeChecker) {
+		// const classNamespace = classDeclaration.
+		// const className =
+		const jsDocs = ts.getJSDocTags(classDeclaration);
+		const namespaceDoc = jsDocs.find(jsDoc => jsDoc.tagName.escapedText === "namespace");
+		const namespace = namespaceDoc?.comment ?? "";
+		const classNameLastPart = classDeclaration.name?.escapedText ?? "";
+		super(`${namespace}.${classNameLastPart}`);
+
+		const heritageClause = classDeclaration.heritageClauses?.find(heritage => {
+			return heritage.token == ts.SyntaxKind.ExtendsKeyword;
+		});
+		if (heritageClause) {
+			// const [parentSymbol] = parentHeritageClause.types.map(typeNode => {
+			// const type = typeChecker.getTypeFromTypeNode(typeNode);
+			// return typeChecker.typeToString(type);
+			// return typeChecker.getSymbolAtLocation(typeNode.expression);
+			// });
+			// const parentName = parentSymbol && typeChecker.getFullyQualifiedName(parentSymbol);
+			const parentType = typeChecker.getTypeFromTypeNode(heritageClause.types[0]);
+			const parentSymbol = parentType.getSymbol();
+			const parentName = parentSymbol && typeChecker.getFullyQualifiedName(parentSymbol);
+			if (parentName?.startsWith("\"sap/")) {
+				const [parentModuleNameQuoted] = parentName.split(".");
+				const parentModuleName = parentModuleNameQuoted.substring(1, parentModuleNameQuoted.length - 1);
+				this.parentClassNameDotNotation = parentModuleName.replace(/\//g, ".");
+			} else {
+				const declarations = parentType
+					.getSymbol()?.declarations?.filter(declaration => ts.isClassDeclaration(declaration));
+				const parentClassDeclaration = declarations?.[0];
+				if (
+					parentClassDeclaration &&
+					(ts.isClassDeclaration(parentClassDeclaration))
+				) {
+					const jsDocs = ts.getJSDocTags(parentClassDeclaration);
+					const namespaceDoc = jsDocs.find(jsDoc => jsDoc.tagName.escapedText === "namespace");
+					const namespace = namespaceDoc?.comment ?? "";
+					const classNameLastPart = parentClassDeclaration.name?.escapedText ?? "";
+					this.parentClassNameDotNotation = `${namespace}.${classNameLastPart}`;
+				}
+			}
+			// if (parentType.isClass()) {
+			// 	const [baseType] = typeChecker.getBaseTypes(parentType);
+			// 	if (baseType?.isClass() && baseType?.getSymbol()) {
+			// 		const symbol = baseType.getSymbol();
+			// 		const parentTypeName = symbol && typeChecker.getFullyQualifiedName(symbol);
+			// 		if (parentTypeName?.startsWith('"sap/')) {
+			// 			const [parentModuleNameQuoted] = parentTypeName.split(".");
+			// 			const parentModuleName = parentModuleNameQuoted.substring(1, parentModuleNameQuoted.length - 1);
+			// 			this.parentClassNameDotNotation = parentModuleName.replace(/\//g, ".");
+			// 		} else {
+			// 			debugger;
+			// 		}
+			// 		// const declarations = baseType.getSymbol().getDeclarations();
+			// 		// const declaration = declarations ? declarations[0] : undefined;
+			// 		// const
+			// 	}
+			// }
+		}
+
+		this.classText = sourceFile.getFullText();
+
+		this._sourceFile = sourceFile;
+		this.fsPath = sourceFile.fileName;
+		this.tsNode = classDeclaration;
+
+		this._fillMethods(classDeclaration, typeChecker);
+		this._fillFields(classDeclaration, typeChecker);
+	}
+
+	private _fillFields(classDeclaration: ts.ClassDeclaration, typeChecker: ts.TypeChecker) {
+		const fields: ts.PropertyDeclaration[] = classDeclaration.members
+			.filter(member => ts.isPropertyDeclaration(member) && ts.isIdentifier(member.name))
+			.map(declaration => declaration as ts.PropertyDeclaration);
+
+		const UIFields: ICustomClassTSField[] = fields.map(field => {
+			const jsDocs = ts.getJSDocTags(field);
+			const ui5IgnoreDoc = jsDocs.find(jsDoc => jsDoc.tagName.escapedText === "ui5ignore");
+			return {
+				ui5ignored: !!ui5IgnoreDoc,
+				owner: this.className,
+
+				static: field.modifiers?.some(modifier => modifier.kind === ts.SyntaxKind.StaticKeyword) ?? false,
+				abstract: field.modifiers?.some(modifier => modifier.kind === ts.SyntaxKind.AbstractKeyword) ?? false,
+				type: typeChecker.typeToString(typeChecker.getTypeAtLocation(field)) ?? "any",
+				visibility:
+					field.modifiers
+						?.find(modifier =>
+							[
+								ts.SyntaxKind.ProtectedKeyword,
+								ts.SyntaxKind.PrivateKeyword,
+								ts.SyntaxKind.PublicKeyword
+							].includes(modifier.kind)
+						)
+						?.getText() ?? "public",
+				name: field.name.getText(),
+				deprecated: jsDocs.some(jsDoc => ts.isJSDocDeprecatedTag(jsDoc)),
+				description: "",
+				isEventHandler: false,
+				tsNode: field
+			};
+		});
+
+		this.fields.push(...UIFields);
+	}
+
+	private _fillMethods(classDeclaration: ts.ClassDeclaration, typeChecker: ts.TypeChecker) {
+		const methods: ts.MethodDeclaration[] = classDeclaration.members
+			.filter(member => ts.isMethodDeclaration(member) && ts.isIdentifier(member.name))
+			.map(declaration => declaration as ts.MethodDeclaration);
+
+		const UIMethods: ICustomClassTSMethod[] = methods.map(method => {
+			const jsDocs = ts.getJSDocTags(method);
+			const ui5IgnoreDoc = jsDocs.find(jsDoc => jsDoc.tagName.escapedText === "ui5ignore");
+			return {
+				ui5ignored: !!ui5IgnoreDoc,
+				owner: this.className,
+				static: method.modifiers?.some(modifier => modifier.kind === ts.SyntaxKind.StaticKeyword) ?? false,
+				abstract: method.modifiers?.some(modifier => modifier.kind === ts.SyntaxKind.AbstractKeyword) ?? false,
+				returnType:
+					typeChecker.typeToString(
+						typeChecker.getTypeAtLocation(method).getCallSignatures()[0]?.getReturnType()
+					) ?? "void",
+				visibility:
+					method.modifiers
+						?.find(modifier =>
+							[
+								ts.SyntaxKind.ProtectedKeyword,
+								ts.SyntaxKind.PrivateKeyword,
+								ts.SyntaxKind.PublicKeyword
+							].includes(modifier.kind)
+						)
+						?.getText() ?? "public",
+				params: method.parameters.map(param => {
+					return {
+						name: param.name.getText(),
+						type: typeChecker.typeToString(typeChecker.getTypeAtLocation(param)) ?? "any",
+						description: "",
+						isOptional: false
+					};
+				}),
+				name: method.name.getText(),
+				deprecated: jsDocs.some(jsDoc => ts.isJSDocDeprecatedTag(jsDoc)),
+				description: "",
+				isEventHandler: false,
+				tsNode: method
+			};
+		});
+
+		this.methods.push(...UIMethods);
+	}
+
+	setCache<Type>(cacheName: string, cacheValue: Type) {
+		this._cache[cacheName] = cacheValue;
+	}
+
+	getCache<Type>(cacheName: string): Type {
+		return <Type>this._cache[cacheName];
+	}
+}
