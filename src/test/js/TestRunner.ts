da3027860cfd3c16c0b234303a3d716c23ee9758
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2023-05-16 23:16:38 +0300

    test code fail

--- src/test/js/TestRunner.ts
+++ src/test/js/TestRunner.ts
@@ -3,22 +3,21 @@ import * as path from "path";
 import { runTests } from "@vscode/test-electron";
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
+	// The folder containing the Extension Manifest package.json
+	// Passed to `--extensionDevelopmentPath`
+	const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
 
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-		const testWorkspace = path.resolve(__dirname, "../../../src/test/js/project");
+	// The path to the extension test script
+	// Passed to --extensionTestsPath
+	const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+	const testWorkspace = path.resolve(__dirname, "../../../src/test/js/project");
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace, "--disable-extensions"] });
-	} catch (err) {
-		console.error("Failed to run tests");
-		process.exit(1);
-	}
+	// Download VS Code, unzip it and run the integration test
+	await runTests({
+		extensionDevelopmentPath,
+		extensionTestsPath,
+		launchArgs: [testWorkspace, "--disable-extensions"]
+	});
 }
 
 main();
